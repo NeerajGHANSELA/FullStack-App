0d768efe984eae60ad9c148d8d305fa73f53c2a9
Neeraj Ghansela <neerajghansela0@gmail.com>
2025-07-06 11:36:44 -0600

    Deleted about, contact-us, demo, pricing, forgotPassword, verfication folders from the app

--- src/app/demo/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Demo() {
-    return (
-        <section className='flex justify-center min-w-screen min-h-screen'>
-            <h1 className='font-bold text-black text-2xl'>In Progress ...</h1>
-        </section>
-    );
-}
